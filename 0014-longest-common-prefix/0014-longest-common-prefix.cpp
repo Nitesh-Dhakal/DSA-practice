47f8f225955aa82f525eec282e12b2c218ecdcb7
Nitesh-Dhakal <niteshdhakal333@gmail.com>
2026-08-01 01:45:27 +0530

    Time: 0 ms (100%), Space: 12 MB (38.77%) - LeetHub

--- 0014-longest-common-prefix/0014-longest-common-prefix.cpp
+++ 0014-longest-common-prefix/0014-longest-common-prefix.cpp
@@ -6,15 +6,12 @@ public:
         string ans = "";
         for(int k=0;k<i;k++){
             char first=strs[0][k];
-         //   if(s)
             for(int l=1;l<j;l++){
-                if(first!=strs[l][k]){
-                    
+                if(k == strs[l].size() || first!=strs[l][k]){
                     return ans;
                 }
             }ans=ans+first;
         }
-        
         return ans;
     }
     
