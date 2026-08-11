b1f789807bf7f5538b83d64e4c42a937763b6903
Nitesh-Dhakal <niteshdhakal333@gmail.com>
2026-08-12 04:16:22 +0530

    Time: 0 ms (100%), Space: 11.7 MB (7.01%) - LeetHub

--- 1732-find-the-highest-altitude/1732-find-the-highest-altitude.cpp
+++ 1732-find-the-highest-altitude/1732-find-the-highest-altitude.cpp
@@ -3,9 +3,9 @@ public:
     int largestAltitude(vector<int>& gain) {
         int n = gain.size();
         vector<int>prefix(n+1);
-        prefix[0]=gain[0];
-        for(int i=1;i<gain.size();i++){
-            prefix[i]=prefix[i-1]+gain[i];
+        prefix[0]=0;
+        for(int i=0;i<gain.size();i++){
+            prefix[i+1]=prefix[i]+gain[i];
         }
         int maxValue = *max_element(prefix.begin(), prefix.end());
         return maxValue;
