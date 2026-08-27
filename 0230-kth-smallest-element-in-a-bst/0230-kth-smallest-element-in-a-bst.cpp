094eee9632c4779ebf3e0e6be04397f714bd256e
Nitesh-Dhakal <niteshdhakal333@gmail.com>
2026-08-28 02:02:02 +0530

    Time: 0 ms (100%), Space: 24.4 MB (40.76%) - LeetHub

--- 0230-kth-smallest-element-in-a-bst/0230-kth-smallest-element-in-a-bst.cpp
+++ 0230-kth-smallest-element-in-a-bst/0230-kth-smallest-element-in-a-bst.cpp
@@ -1,19 +1,20 @@
 class Solution {
 public:
-    int counter=0;
+    int counter = 0;
     int finalanswer;
-    void helper(TreeNode* root, int k){
-        if(root==NULL)return ;
-        helper(root->left,k);
+    void helper(TreeNode* root, int k) {
+        if (root == NULL || counter >= k)
+            return;
+        helper(root->left, k);
         counter++;
-        if(counter==k){
-            finalanswer=root->val;
+        if (counter == k) {
+            finalanswer = root->val;
             return;
         }
-        helper(root->right,k);
+        helper(root->right, k);
     }
     int kthSmallest(TreeNode* root, int k) {
-        helper(root,k);
+        helper(root, k);
         return finalanswer;
     }
 };
