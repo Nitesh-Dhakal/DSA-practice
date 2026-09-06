042f41fd11af061bf0ac585f34849dc4e66d208a
Nitesh-Dhakal <niteshdhakal333@gmail.com>
2026-09-07 02:15:37 +0530

    Time: 3 ms (71.54%), Space: 19.2 MB (15.09%) - LeetHub

---
+++ 0210-course-schedule-ii/0210-course-schedule-ii.cpp
@@ -0,0 +1,35 @@
+class Solution {
+public:
+    vector<int>result;
+    bool dfs(vector<vector<int>>& adj,int u,vector<bool>& visited,vector<bool>&pathvisited){
+        visited[u]=true;
+        pathvisited[u]=true;
+        for(int neigh:adj[u]){
+            if(visited[neigh]==false){
+                if(dfs(adj,neigh,visited,pathvisited)==true)return true;
+            }
+            else if(pathvisited[neigh]==true)return true;
+        }
+        result.push_back(u);
+        pathvisited[u]=false;
+        return false;
+    }
+    vector<int> findOrder(int num, vector<vector<int>>& edges) {
+        result.clear();
+        vector<vector<int>>adj(num);
+        vector<bool>visited(num,false);
+        vector<bool>pathvisited(num ,false);
+        for(auto edge:edges){
+            int u=edge[1];
+            int v=edge[0];
+            adj[u].push_back(v);
+        }
+        for(int i=0;i<num;i++){
+            if(visited[i]==false){
+               if( dfs(adj,i,visited,pathvisited)==true)return{};
+            }           
+        }
+        reverse(result.begin(),result.end());
+        return result;
+    }
+};
