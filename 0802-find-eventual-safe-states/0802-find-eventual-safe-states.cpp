60cad2e09442e28452d79f61315450ac43a202ff
Nitesh-Dhakal <niteshdhakal333@gmail.com>
2026-09-07 02:47:00 +0530

    Time: 7 ms (81.14%), Space: 51.8 MB (91.95%) - LeetHub

---
+++ 0802-find-eventual-safe-states/0802-find-eventual-safe-states.cpp
@@ -0,0 +1,32 @@
+class Solution {
+public:
+    vector<int>result;
+    bool dfs(int u,vector<vector<int>>& graph,vector<bool>& visited, vector<bool>& pathvisited,vector<bool>& safe){
+        visited[u]=true;
+        pathvisited[u]=true;
+        for(int neigh:graph[u]){
+            if(visited[neigh]==false){
+                if(dfs(neigh,graph,visited,pathvisited,safe)==true)return true;
+            }
+            else if(pathvisited[neigh]==true)return true;
+        }
+        result.push_back(u);
+        safe[u]=true;
+        pathvisited[u]=false;
+        return false;
+    }
+    vector<int> eventualSafeNodes(vector<vector<int>>& graph) {
+        result.clear();
+        int n = graph.size();
+        vector<bool>visited(n,false);
+        vector<bool>pathvisited(n,false);
+        vector<bool>safe(n,false);
+        for(int i=0;i<n;i++){
+            if(visited[i]==false){
+                dfs(i,graph,visited,pathvisited,safe);
+            }
+        }
+        sort(result.begin(),result.end());
+        return result;
+    }
+};
